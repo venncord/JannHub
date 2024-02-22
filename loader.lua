15dc751aae04f5cb4aad630960db91bf8ade2f4d
LeanClient <108145692+LeanClient@users.noreply.github.com>
2024-02-22 17:52:53 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -85,7 +85,7 @@ local ToggleVotesThing = Tab:CreateToggle({
 
 local Toggle = Tab:CreateToggle({
    Name = "Auto Win Obbies",
-   Info = "Speaks for itself.", -- Speaks for itself, Remove if none.
+   Info = "Speaks for itself. burps", -- Speaks for itself, Remove if none.
    CurrentValue = false,
    Flag = "Toggle12", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
