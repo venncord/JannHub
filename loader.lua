2133a4d685ed095a46de41016eefd66a3f40f19e
LeanClient <108145692+LeanClient@users.noreply.github.com>
2024-02-22 22:11:06 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -127,7 +127,7 @@ local Toggle = Tab:CreateToggle({
    Flag = "Toggle12", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
          while true do
-            task.wait(0.00001)
+            task.wait(1)
 						if Value == true then
             for i,v in pairs (workspace:GetDescendants()) do
                 if v.Name == "Finish" then
